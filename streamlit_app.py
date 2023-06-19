4b84ba35f052f8e6a87d0498530d5a0a55c6ba6d
GabrielTalasso <gukstin@gmail.com>
2023-06-19 16:04:02 -0300

    new file:   streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+import altair as alt
+import pandas as pd
+import streamlit as st
+from vega_datasets import data
+
+st.set_page_config(
+    page_title="Trabalho Final Séries Temporais - 2s 2023", page_icon="⬇", layout="centered"
+)
+
+
+st.title("Trabalho final Séries Temporais")
+
+st.write("Começo dos testes para criação do DashBoard")
