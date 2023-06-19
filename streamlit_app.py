a863f3c6f2dc5776ca9326d26eb808239d1fdda7
GabrielTalasso <gukstin@gmail.com>
2023-06-19 17:07:54 -0300

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,6 @@ from statsmodels.tsa.seasonal import seasonal_decompose
 from sklearn.model_selection import TimeSeriesSplit
 from sklearn.metrics import mean_squared_error
 
-!pip install statsforecast
 from statsforecast.models import HistoricAverage
 from statsforecast.models import Naive
 from statsforecast.models import RandomWalkWithDrift
@@ -74,91 +73,47 @@ with st.sidebar:
 st.title('Trabalho Final Séries Temporais')
 st.subheader('Grupo: Gabriel Ukstin Talasso - 235078 ; ....')
 st.markdown("## Visão geral dos dados")
+st.markdown("### Alterne entre as abas para as visualizações")
 
 @st.cache_data
 def read_data():
-    URL = "https://raw.githubusercontent.com/marcopeix/streamlit-population-canada/master/data/quarterly_canada_population.csv"
-    df = pd.read_csv(URL, index_col=0, dtype={'Quarter': str, 
-                                          'Canada': np.int32,
-                                         'Newfoundland and Labrador': np.int32,
-                                          'Prince Edward Island': np.int32,
-                                          'Nova Scotia': np.int32,
-                                          'New Brunswick': np.int32,
-                                          'Quebec': np.int32,
-                                          'Ontario': np.int32,
-                                          'Manitoba': np.int32,
-                                          'Saskatchewan': np.int32,
-                                          'Alberta': np.int32,
-                                          'British Columbia': np.int32,
-                                          'Yukon': np.int32,
-                                          'Northwest Territories': np.int32,
-                                          'Nunavut': np.int32})
+        # Set time period
+    start = datetime(2010, 1, 1)
+    end = pd.to_datetime(datetime.now().strftime("%Y-%m-%d"))
+    # Create Point for Vancouver, BC
+    vancouver = Point(49.2497, -123.1193, 70)
+    #campinas = Point(-22.9056, -47.0608, 686)
+    #saopaulo = Point(-23.5475, -46.6361, 769)
 
-    return df
+    # Get daily data for 2018
+    data = Daily(vancouver, start, end)
+    data = data.fetch()
+    data = data[['tavg', 'prcp']]
 
-df= read_data()
-if df not in st.session_state:
-    st.session_state['df'] = df
-st.write(df)
+    return data
 
-col1, col2 = st.columns(2)
+data = read_data()
+returns = data['tavg']
 
-quarter_option = col1.selectbox(label='Quarter', options=['Q1', 'Q2', 'Q3', 'Q4'])
-year_option = col2.selectbox(label='Year', options=np.arange(1991, 2024, 1)[::-1])
+if data not in st.session_state:
+    st.session_state['df'] = data
 
-try:
-    
-    fig = px.choropleth(
-        geo_df,
-        geojson=geo,
-        # locations=['Canada','Newfoundland and Labrador','Prince Edward Island','Nova Scotia','New Brunswick','Quebec','Ontario','Manitoba','Saskatchewan','Alberta','British Columbia','Yukon','Northwest Territories','Nunavut'],
-        locations=geo_df.index,
-        featureidkey="properties.name",
-        color=f"{quarter_option} {year_option}",
-        color_continuous_scale='oranges'
-    )
 
-    fig.update_layout(margin={"r": 0, "t":0, "l":0, "b":0})
-    fig.update_geos(fitbounds="locations", visible=True)
+tab1, tab2, tab3 = st.tabs(["Tabela dos dados", 
+                      "Grafico da Série", 
+                      "Grafico Diferenciada"])
 
+with tab1:
+    st.write(data)
+
+with tab2:
+    fig = px.line(returns, title='Temperatura Média Diária', 
+                  labels=({'value':'Temperatura Média', 'time':'Data'}))
+    fig.update_layout(showlegend=False)
     st.plotly_chart(fig)
-except ValueError:
-    st.error('No data available for your quarter and year selection')
 
-tab1, tab2 = st.tabs(['Line plot', 'Bar plot'])
-
-province_option = tab1.selectbox(label='Select a province or a territory', options=df.columns[1:])
-line_fig, line_ax = plt.subplots()
-line_ax.plot(df[f'{province_option}'])
-line_ax.set_xticks(np.arange(2, len(df), 8))
-line_ax.set_xticklabels(np.arange(1992, 2024, 2))
-line_ax.yaxis.set_major_locator(plt.MaxNLocator(10))
-line_fig.autofmt_xdate()
-tab1.subheader(f"{province_option}")
-tab1.pyplot(line_fig)
-
-@st.cache_data
-def format_data_for_animated_barplot(df):
-    anim_bar_df = pd.DataFrame()
-
-    for each in df.columns[1:]:
-        temp_df = pd.DataFrame({'pop': df[f'{each}'].values, 'name': f'{each}'})
-        anim_bar_df = pd.concat([anim_bar_df, temp_df])
-
-    dates = []
-    qs = df.index.values
-    for _ in range(13):
-        dates.extend(qs)
-    anim_bar_df['date'] = dates
-
-    return anim_bar_df
-
-anim_bar_df = format_data_for_animated_barplot(df)
-bar_fig = px.bar(anim_bar_df, 
-                 x='name', 
-                 y='pop', 
-                 color='name', 
-                 animation_frame='date', 
-                 animation_group='name', 
-                 range_y=[0, 16000000])
-tab2.plotly_chart(bar_fig)
+with tab3:
+    fig = px.line(returns.diff(1).dropna(), title='Temperatura Média Diária - Diferenciada',
+                  labels = {'value':'Diferença da temperatura', 'time':'Data'})
+    fig.update_layout(showlegend=False)
+    st.plotly_chart(fig)
